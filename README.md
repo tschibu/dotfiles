976a1cae8a59b1357aaf8405c99f3717f6dac5dd
Remo Schwarzentruber <Remo.Schwarzentruber@stud.hslu.ch>
2019-09-23 07:09:52 +0200

    add zshrc and custom functions

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# dotfiles
-my dotfiles repo
+# Getting Started With Dotfiles <3
+
+```bash
+~/.*
+```
+
+**Install zsh:**
+
+```bash
+brew install zsh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+```
+
+
+**Link config files:**
+
+```bash
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/oh-my-zsh/hslu.zsh  ~/.oh-my-zsh/custom/hslu.zsh
+ln -s ~/dotfiles/oh-my-zsh/private.zsh  ~/.oh-my-zsh/custom/private.zsh
+```
---
+++ oh-my-zsh/hslu.zsh
@@ -0,0 +1,27 @@
+# HSLU Projects
+function wpren() {
+  if [ -d /Users/tluscre1/Documents/Studium.Local/PREN ]; then
+    cd /Users/tluscre1/Documents/Studium.Local/PREN
+    # define alias's
+    alias pi='ssh pi@172.20.10.10'
+    alias pilu='ssh pi@192.168.1.117'
+    alias piurs='ssh pi@192.168.43.3'
+    source activate molly
+  fi
+}
+
+function wipcv() {
+  if [ -d /Users/tluscre1/Documents/Studium.Local/IPCV/repo ]; then
+    cd /Users/tluscre1/Documents/Studium.Local/IPCV/repo
+    source activate molly
+    jupyter notebook
+  fi
+}
+
+function wdl4g() {
+  if [ -d /Users/tluscre1/Documents/Studium.Local/DL4G/repo ]; then
+    cd /Users/tluscre1/Documents/Studium.Local/DL4G/repo
+    source activate dl4g
+    jupyter notebook &
+  fi
+}
