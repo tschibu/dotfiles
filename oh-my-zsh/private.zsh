a025503d702cd42cbe7b6a6e71519c8d62ff959c
tschibu <tschibu@gmail.com>
2024-03-11 09:40:00 +0100

    NOTICKET: Update/upgrade Homebrew and their installed packages

--- oh-my-zsh/private.zsh
+++ oh-my-zsh/private.zsh
@@ -5,3 +5,6 @@ alias nas='ssh remo@ds218'
 
 # editor
 alias v=nvim
+
+# Update/upgrade Homebrew and their installed packages
+alias brew-update='brew update; brew upgrade; brew upgrade --cask; brew cleanup'
