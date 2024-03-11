e7acb15adf1b1bc5b3fdcda3fc655bf28db0d7ac
tschibu <tschibu@gmail.com>
2024-03-11 10:10:11 +0100

    NOTICKET: compare vscode extensions (installed vs downloaded)

--- oh-my-zsh/private.zsh
+++ oh-my-zsh/private.zsh
@@ -8,3 +8,16 @@ alias v=nvim
 
 # Update/upgrade Homebrew and their installed packages
 alias brew-update='brew update; brew upgrade; brew upgrade --cask; brew cleanup'
+
+# compare vscode extensions (installed vs downloaded)
+function code-extensions-compare() {
+    # List of all extensions
+    extensions=`cat ~/dotfiles/vscode/extensions_to_install`
+
+    # Get a list of all currently installed extensions.
+    installed_extensions=$(code --list-extensions)
+
+    # compare the extensions lists
+    diff <(echo "$extensions") <(echo "$installed_extensions")
+
+}
